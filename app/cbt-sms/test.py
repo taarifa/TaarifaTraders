6d43abfc2e4e917b03eabfb3de3d353aabcaa716
Fayaz Valli <fayaz.valli95@gmail.com>
2015-04-17 11:58:56 +0200

    deleted test files

--- app/cbt-sms/test.py
+++
@@ -1,9 +0,0 @@
-import telerivet
-
-tr = telerivet.API('1PtmwpYc0OyFVS6F6wIqlQHa1Mb3c4db')
-project = tr.initProjectById('PJ4f75ddf74530bf70')
-
-sent_msg = project.sendMessage(
-    content = "djafl;kjdslfk;ds", 
-    to_number = "+255782978899"
-)
